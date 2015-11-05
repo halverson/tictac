ca2604874b179c65f2b17dc12f0a89e9d2002fd2
Brent Nygaard <nygaardmeister@gmail.com>
2015-11-05 14:37:08 +0200

    Clean up javascript

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
     
-    var player1 = true;
-    var player2 = false;
+    var player1 = true,
+        player2 = false;
     
-    var ClickCell = function() {
+    var checkWin = function () {
+        
+    };
+    
+    var playGame = function () {
         var clickedCell = $(this).children();
+        
         if (player1 === true) {
             clickedCell.addClass("ex");
             player1 = false;
+            player2 = true;
         } else {
             clickedCell.addClass("oh");
             player1 = true;
+            player2 = false;
         }
     };
     
-    $(".square").on('click', ClickCell);
+    $(".square").on('click', playGame);
     
 });
