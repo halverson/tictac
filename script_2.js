93d2d48fa92c96ef64c11b0a30b67f25eeffee74
Brent Nygaard <nygaardmeister@gmail.com>
2015-11-08 16:28:19 +0200

    Add functionality to check if won on the anti-diagonal.

--- script_2.js
+++ script_2.js
@@ -33,33 +33,47 @@ $(document).ready(function () {
     var checkWin = function (player, row, col) {
         
         /*Check if there is a win in the current row*/
-        for (var i = 0; i < board[row].length; i++) {
-            if (board[row][i] === player && board[row][i + 1] === player && board[row][i + 2] === player) {
-                return true;
-            } else {
+        for (var i = 0; i < board.length; i++) {
+            if (board[row][i] != player) {
                 break;
+            }
+            if (i == board.length - 1) {
+                return true;
             }
         }
         
         /*Check if there is a win in the current column*/
-        for (var i = 0; i < board[row].length; i++) {
-            if (board[i][col] === player && board[i + 1][col] === player && board[i + 2][col] === player) {
-                return true;
-            } else {
+        for (var i = 0; i < board.length; i++) {
+            if (board[i][col] != player) {
                 break;
+            }
+            if (i == board.length - 1) {
+                return true;
             }
         }
         
-        /*Check if there is a win in the diagonal*/
+        /*Check if there is a win in the \ diagonal*/
         if (row === col) {
-            for (var i = 0; i < board[row].length; i++) {
-                if (board[i][i] === player && board[i+1][i+1] === player && board[i+2][i+2] === player) {
-                    return true;
-                } else {
+            for (var i = 0; i < board.length; i++) {
+                if (board[i][i] != player) {
                     break;
+                }
+                if (i == board.length - 1) {
+                    return true;
                 }
             }
         }
+        
+        /*Check if there is a win in the anti-diagonal*/
+        for (var i = 0; i < board.length; i++) {
+            if (board[i][(board.length - 1) - i] != player) {
+                break;
+            }
+            if (i == board.length - 1) {
+                return true;
+            }
+        }
+        
     };
     
     var playerMove = function (a, b, c) { /*Draw appropriate token and update *board* array.*/
