a6724987bff2c5506149890deb608ad800cf268c
Brent Nygaard <nygaardmeister@gmail.com>
2015-11-08 16:05:08 +0200

    Add function to check if game won by row, column or forward diagonal.

--- script_2.js
+++ script_2.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     
     var player1 = true,
         player2 = false,
+        moveCounter = 0,
+        displayText = $("#info"),
+        restartBttn = $("#restart"),
         
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     
@@ -27,17 +30,71 @@ $(document).ready(function () {
         }
     };
     
-    var playerMove = function (a, b, c) { /*Draw appropriate token and update *board* array.*/
-        if (player1 === true) {
-            a.text("X");
-            board[b][c] = 1;
-        } else {
-            a.text("O");
-            board[b][c] = 2;
+    var checkWin = function (player, row, col) {
+        
+        /*Check if there is a win in the current row*/
+        for (var i = 0; i < board[row].length; i++) {
+            if (board[row][i] === player && board[row][i + 1] === player && board[row][i + 2] === player) {
+                return true;
+            } else {
+                break;
+            }
         }
         
-        console.log("row: " + b + " col: " + c);
-        changePlayer();
+        /*Check if there is a win in the current column*/
+        for (var i = 0; i < board[row].length; i++) {
+            if (board[i][col] === player && board[i + 1][col] === player && board[i + 2][col] === player) {
+                return true;
+            } else {
+                break;
+            }
+        }
+        
+        /*Check if there is a win in the diagonal*/
+        if (row === col) {
+            for (var i = 0; i < board[row].length; i++) {
+                if (board[i][i] === player && board[i+1][i+1] === player && board[i+2][i+2] === player) {
+                    return true;
+                } else {
+                    break;
+                }
+            }
+        }
+    };
+    
+    var playerMove = function (a, b, c) { /*Draw appropriate token and update *board* array.*/
+        
+        var p;
+        
+        if (player1 === true) {
+            p = 1;
+            a.text("X");
+            board[b][c] = p;
+            
+        } else {
+            p = 2;
+            a.text("O");
+            board[b][c] = p;
+        }
+        
+        moveCounter++;
+        
+        if (checkWin(p, b, c) === true) {
+            if (player1 === true) {
+                    displayText.text("X has won the game!");
+                } else {
+                    displayText.text("O has won the game!");
+                }
+                restartBttn.css("visibility", "visible");
+        } else if (moveCounter == 9) {
+            displayText.text("It's a draw! Play again:");
+            restartBttn.css("visibility", "visible");
+        } else {
+            changePlayer();
+        }
+        
+        console.log("Played " + a.text() + " At row: " + b + " col: " + c);
+        console.log("Turn " + moveCounter);
     };
     
     drawBoard();
@@ -50,18 +107,19 @@ $(document).ready(function () {
         if (board[spaceRow][spaceCol] === 0) { /*Check if the clicked square is empty.*/
             playerMove(chosenSpace, spaceRow, spaceCol);
         } else {
-            $("#info").text("Choose a different square");
+            displayText.text("Choose a different square");
         }
         
     });
     
-    $("#restart").click(function () { /*Resets game to starting state when called.*/
+    $("#restart").click(function () { /*Resets game to starting state when clicked.*/
         player1 = true;
         player2 = false;
+        moveCounter = 0;
         var x = $(".square").children();
         x.text("");
-        $("#restart").css("visibility", "hidden");
-        $("#info").text("Play On!");
+        restartBttn.css("visibility", "hidden");
+        displayText.text("Play On!");
         for (var i = 0; i < board.length; i++) {
             for (var j = 0; j < board[i].length; j++) {
                 board[i][j] = 0;
