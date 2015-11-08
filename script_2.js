d2988db2c3a46370fdb3adf08c0dbe88dca71800
Brent Nygaard <nygaardmeister@gmail.com>
2015-11-08 12:58:02 +0200

    Add comments and check if chosen space is empty before drawing token.

--- script_2.js
+++ script_2.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     
     var drawBoard = function () {
+        
+        /*Draw board div's and h1's with row and col attributes based on *board* array.*/
         for (var i = 0; i < board.length; i++) {
             for (var j = 0; j < board[i].length; j++) {
                 $("#board").append("<div class='square'><h1></h1></div>");
@@ -15,7 +17,7 @@ $(document).ready(function () {
         }
     };
     
-    var changePlayer = function () {
+    var changePlayer = function () { /*Change who's turn it is when called.*/
         if (player1 === true) {
             player1 = false;
             player2 = true;
@@ -25,11 +27,7 @@ $(document).ready(function () {
         }
     };
     
-    /*var isFull = function (element, index, array) {
-        return element == 1;
-    };*/
-    
-    var playerMove = function (a, b, c) {
+    var playerMove = function (a, b, c) { /*Draw appropriate token and update *board* array.*/
         if (player1 === true) {
             a.text("X");
             board[b][c] = 1;
@@ -38,22 +36,26 @@ $(document).ready(function () {
             board[b][c] = 2;
         }
         
-        console.log(board);
+        console.log("row: " + b + " col: " + c);
         changePlayer();
     };
     
     drawBoard();
 
     $(".square").click(function () {
-        var spaceRow = $(this).attr("row");
-        var spaceCol = $(this).attr("col");
-        var chosenSpace = $(this).children();
+        var spaceRow = $(this).attr("row"); /*Get the clicked square's row number.*/
+        var spaceCol = $(this).attr("col"); /*Get the clicked square's column number.*/
+        var chosenSpace = $(this).children(); /*Target the h1 tag within the clicked square.*/
         
-        playerMove(chosenSpace, spaceRow, spaceCol);
+        if (board[spaceRow][spaceCol] === 0) { /*Check if the clicked square is empty.*/
+            playerMove(chosenSpace, spaceRow, spaceCol);
+        } else {
+            $("#info").text("Choose a different square");
+        }
         
     });
     
-    $("#restart").click(function () {
+    $("#restart").click(function () { /*Resets game to starting state when called.*/
         player1 = true;
         player2 = false;
         var x = $(".square").children();
@@ -66,7 +68,7 @@ $(document).ready(function () {
             }
         }
         
-        console.log("New Game! " + board);
+        console.log("New Game!");
     });
     
 });
